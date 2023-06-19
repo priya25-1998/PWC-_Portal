7b9bf3e63e02ea5dc4d188e26e1d11036fce64af
Varsha C N <varsha.cn@metricstream.com>
2023-06-19 09:45:40 +0530

    Upload New File

---
+++ src/pages/home/Container.js
@@ -0,0 +1,16 @@
+import { Box } from '@mui/material'
+import React from 'react'
+
+export function Container(children){
+  return (
+    <Box style={{
+      display: 'flex',
+      backgroundColor: 'white',
+      height: '100vh',
+      width: '100%'
+    }}>
+      {[]}
+    </Box>
+  )
+}
+
