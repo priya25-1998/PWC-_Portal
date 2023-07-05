46430205031aee99f73ec8a935360f1ce65af930
Srikumar <srikumar@metricstream.com>
2023-07-05 13:56:18 +0530

    Updated AG Grid details as per the new request

--- src/components/Resources/AgGridReport.js
+++ src/components/Resources/AgGridReport.js
@@ -1,20 +1,20 @@
-
 import React, { useState, useEffect, useMemo } from "react";
 import { AgGridReact } from "ag-grid-react";
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-alpine.css";
-import "ag-grid-enterprise";
-import { LicenseManager } from "ag-grid-enterprise";
-import "../../App.css";
 import ReportDetails from "./ReportDetails.json";
+import { LicenseManager } from "ag-grid-enterprise";
 
-// LicenseManager.setLicenseKey("MTY4ODA3OTYwMDAwMA==5266e2b19455d0b5c7c8c52bf6367755");
+LicenseManager.setLicenseKey(
+  "MTY4ODA3OTYwMDAwMA==5266e2b19455d0b5c7c8c52bf6367755"
+);
 
 function AgGridReport() {
   const [rowData, setRowData] = useState([]);
   const [headers, setHeaders] = useState([]);
   const [reportName, setReportName] = useState("");
-  const [appliedFilters, setAppliedFilters] = useState([]);
+  const [appliedFilterCardFilters, setAppliedFilterCardFilters] = useState([]);
+  const [rootFilter, setRootFilter] = useState({});
   const [originalData, setOriginalData] = useState([]);
 
   useEffect(() => {
@@ -34,7 +34,6 @@ function AgGridReport() {
       if (report) {
         const { reportName, url } = report;
         setReportName(reportName);
-        console.log("urlllll:::::::::",url);
         fetchCsvData(url)
           .then((rowData) => {
             setRowData(rowData);
@@ -85,38 +84,32 @@ function AgGridReport() {
       filteredData = filteredData.filter((row) => row[key] === filters[key]);
     }
     setRowData(filteredData);
-    setAppliedFilters(Object.entries(filters));
+    setAppliedFilterCardFilters(Object.entries(filters));
+    setRootFilter({ ...rootFilter, ...filters });
   };
 
-  const onFilterChanged = (event) => {
-    const api = event.api;
-    const allFilters = api.getFilterModel();
-
-    const newAppliedFilters = Object.entries(allFilters).map(([field, filter]) => ({
-      field,
-      filter: filter.filter,
-    }));
-
-    setAppliedFilters(newAppliedFilters);
-
-    // Apply the filters
+  const applyFilterCardFilters = (filters) => {
     let filteredData = originalData;
-    for (const { field, filter } of newAppliedFilters) {
+    for (const key in filters) {
       filteredData = filteredData.filter((row) =>
-        row[field].toString().toLowerCase().includes(filter.toLowerCase())
+        row[key].toString().toLowerCase().includes(filters[key].toLowerCase())
       );
     }
-
     setRowData(filteredData);
+    setAppliedFilterCardFilters(Object.entries(filters));
+    setRootFilter({ ...rootFilter, ...filters });
   };
 
   const removeFilter = (field) => {
-    const newAppliedFilters = appliedFilters.filter(([filterField]) => filterField !== field);
-    setAppliedFilters(newAppliedFilters);
+    const newFilterCardFilters = appliedFilterCardFilters.filter(([filterField]) => filterField !== field);
+    const newRootFilter = { ...rootFilter };
+    delete newRootFilter[field];
 
-    // Apply the filters
+    setAppliedFilterCardFilters(newFilterCardFilters);
+    setRootFilter(newRootFilter);
+
     let filteredData = originalData;
-    for (const [filterField, filter] of newAppliedFilters) {
+    for (const [filterField, filter] of newFilterCardFilters) {
       filteredData = filteredData.filter((row) =>
         row[filterField].toString().toLowerCase().includes(filter.toLowerCase())
       );
@@ -141,17 +134,16 @@ function AgGridReport() {
   return (
     <div id="root" className="ag-theme-alpine" style={{ height: "100vh", width: "99vw" }}>
       <div className="report-header">
-        
-        <div>
+        {/* <div>
           <button className="back-button" onClick={goBack}>
             &lt;
           </button>
-        </div>
+        </div> */}
         <h2 className="report-name">{reportName}</h2>
       </div>
 
       <div className="filter-cards">
-        {appliedFilters.map(([field, filter]) => (
+        {appliedFilterCardFilters.map(([field, filter]) => (
           <div key={field} className="filter-card">
             <span>{field}:</span>
             <span>{filter}</span>
@@ -161,14 +153,34 @@ function AgGridReport() {
           </div>
         ))}
       </div>
-      
+
       <AgGridReact
         rowData={rowData}
-        columnDefs={headers.map((header) => ({
-          field: header,
-          headerName: header,
-          filter: true,
-        }))}
+        columnDefs={[
+          {
+            field: "", 
+            headerName: "", 
+            checkboxSelection: true, 
+            headerCheckboxSelection: true, 
+            rowDrag: true, 
+            width: 50, 
+            minWidth: 50, 
+            maxWidth: 50, 
+          },
+          ...headers.map((header) => ({
+            field: header,
+            headerName: header,
+            filter: true,
+            // chartDataType: "category",
+            chartDataType: "series",
+            enableRowGroup: true,
+            enablePivot: true,
+            enableValue: true,
+            width: 200,
+            minWidth: 100,
+            maxWidth: 400,
+          })),
+        ]}
         defaultColDef={{
           sortable: true,
           resizable: true,
@@ -216,25 +228,14 @@ function AgGridReport() {
           statusPanels: [
             { statusPanel: "agTotalRowCountComponent", align: "left" },
             { statusPanel: "agSelectedRowCountComponent", align: "left" },
+            // { statusPanel: "agAggregationComponent", align: "right" },
           ],
         }}
-        onRowClicked={(event) => {
-          navigateToDetails(event.data.id, event.data.url);
-        }}
-        onFilterChanged={onFilterChanged}
-        getPopupParent={() => popupParent}
+        onFirstDataRendered={(params) => params.api.sizeColumnsToFit()}
+        popupParent={popupParent}
       />
     </div>
   );
 }
 
 export default AgGridReport;
-
-
-
-
-
-
-
-
-
